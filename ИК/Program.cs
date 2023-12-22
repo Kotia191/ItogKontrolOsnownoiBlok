3dc8490547a30a829b5833d3903d9daf279552c6
Katerina <kotia191@mail.ru>
2023-12-23 01:06:00 +0900

    записала решение задачи

---
+++ ИК/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите строки через запятую:");
+string input = Console.ReadLine();
+
+string[] originalArray = input.Split(',');
+
+int count = 0;
+for (int i = 0; i < originalArray.Length; i++)
+{
+    if (originalArray[i].Length <= 3)
+    {
+        count++;
+    }
+}
+
+string[] newArray = new string[count];
+int index = 0;
+for (int i = 0; i < originalArray.Length; i++)
+{
+    if (originalArray[i].Length <= 3)
+    {
+        newArray[index++] = originalArray[i];
+    }
+}
+
+Console.Write("Новый массив: ");
+for (int i = 0; i < newArray.Length; i++)
+{
+    Console.Write(newArray[i] + " ");
+}
+
+
